a143107bdc66df36e77b2703c2a4953172314859
Brian Nicolellis <briannicolellis@prodigy.net>
2024-03-13 17:02:47 -0500

    Added tests for calculating interest

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Bank.cpp"
+
+int main() {
+    int principle = 10000;
+    int time = 7;
+    float rate = 6.25;
+
+    float result = conSimpleInterest(principle, time, rate);
+    std::cout << result << "\n";
+}
