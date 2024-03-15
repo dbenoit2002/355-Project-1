7b091c3a908076f2d8d0b5bdf8e65758b76d255c
noahP13 <noahpearson13@gmail.com>
2024-03-15 00:19:36 -0500

    Updated Main File
    Added some basic user input aspects that cover some of my rules

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 #include "Bank.cpp"
+#include "InputMessing.cpp"
+using namespace std;
 
 int main() {
     int principle = 10000;
@@ -7,5 +10,25 @@ int main() {
     float rate = 6.25;
 
     float result = conSimpleInterest(principle, time, rate);
-    std::cout << result << "\n";
+    cout << result << "\n";
+
+    //STR50
+    string userInput;
+    cout << "Input a string: \n";
+    cin >> userInput;
+    while(1)
+    {
+        //STR51
+        if(userInput == "")
+        {
+            cin.clear();
+            cout<<"Null Input, please enter a string"<< endl;
+            cin>>userInput;
+        }else{
+            break;
+        }
+    }
+    
+
+
 }
