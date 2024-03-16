344e0e6b732a614d3d6ef8f3326fe3072fc239f4
Brian Nicolellis <briannicolellis@prodigy.net>
2024-03-16 11:27:35 -0500

    Updated user class, Added calculate interest function

--- Bank.cpp
+++ Bank.cpp
@@ -4,8 +4,52 @@
 #include <vector>
 #include <math.h>
 #include <string>
+#include <type_traits>
 
 #include "Bank.h"
+
+User::User(std::string name, int id) {
+    this->name = name;
+    this->id = id;
+    balance = 0;
+    principle = 0;
+    rate=0;
+}
+
+float User::withdraw(float amount) {
+    balance -= amount;
+    return balance;
+}
+
+float User::deposit(float amount) {
+    balance += amount;
+    return balance;
+}
+
+std::string User::getName() {
+    return name;
+}
+
+int User::getID() {
+    return id;
+}
+
+float User::getBalance() {
+    return balance;
+}
+
+void User::takeLoan(int amount, float rate) {
+    principle = amount;
+    this->rate = rate;
+}
+
+long User::getPrinciple() {
+    return principle;
+}
+
+long User::getRate() {
+    return rate;
+}
 
 #define compoundInterest(fa, p, t, n, r) \
     /*PRE10*/ \
@@ -37,18 +81,23 @@ float conSimpleInterest(int principle, int time, float rate) {
     return interest; //ERR59
 }
 
-User::User(std::string name, int id) {
-    this->name = name;
-    this->id = id;
-    balance = 0;
-}
+//DCL50, MSC52
+template <typename Arg, typename... Ts, typename std::enable_if<std::is_integral<Arg>::value>::type * = nullptr>
+bool calculateInterest(Arg i, Ts... all) { 
+    long values[] = { i, all... };
+    int size = sizeof(values) / sizeof(values[0]); //ARR01
+    float result;
 
-float User::withdraw(float amount) {
-    balance -= amount;
-    return balance;
-}
-
-float User::deposit(float amount) {
-    balance += amount;
-    return balance;
+    for(int i = 0; i < size; i += 2) {
+        if(i == size) {
+            return false; //not even # args
+        }
+        else {
+            result = conSimpleInterest(values[i], 10, values[i+1]);
+            std::cout << "Simple interest: " << result << "\n";
+            compoundInterest(result, values[i], 10, 4, values[i+1]);
+            std::cout << "Compound interest: " << result << "\n";
+        }
+    }
+    return true;
 }
