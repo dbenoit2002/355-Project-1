8f3fd58b2c5d780188989723c97cf06d80f20642
Brian Nicolellis <briannicolellis@prodigy.net>
2024-03-16 11:27:11 -0500

    Updated user class

--- Bank.h
+++ Bank.h
@@ -1,20 +1,28 @@
 #ifndef BANK_H
 #define BANK_H
 
-
-void interestHelper(float *interest, int principle, int time, float rate, std::mutex *mutex);
-float conSimpleInterest(int principle, int time, float rate);
-
 class User {
     private:
         std::string name;
         int id;
         float balance;
 
+        long principle;
+        long rate;
+
     public:
         User(std::string name, int id);
         float withdraw(float amount);
         float deposit(float amount);
+        std::string getName();
+        int getID();
+        float getBalance();
+        void takeLoan(int amount, float rate);
+        long getPrinciple();
+        long getRate();
 };
+
+void interestHelper(float *interest, int principle, int time, float rate, std::mutex *mutex);
+float conSimpleInterest(int principle, int time, float rate);
 
 #endif
