9e95d777a0758923bf361f241a0975a668709ba7
dbenoit2002 <147451109+dbenoit2002@users.noreply.github.com>
2024-03-16 12:39:54 -0500

    Added range check (rules)

--- main.cpp
+++ main.cpp
@@ -8,4 +8,21 @@ int main() {
 
     float result = conSimpleInterest(principle, time, rate);
     std::cout << result << "\n";
+
+    vector<int> bankAccounts = {0, 1, 2, 3, 4};
+    vector<int>::iterator it = bankAccounts.begin();
+    
+    while(it != bankAccounts.end())
+    {
+        if(it >= bankAccounts.begin() && it < bankAccounts.end())
+        {
+            cout << *it <<endl;
+        }
+        else
+        {
+            cerr<<"Index out of range"<<endl;
+        }
+        advance(it, 1);
+        //do something with accounts
+    }
 }
