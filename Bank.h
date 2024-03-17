6c945623116c02abe21a16e3434ccd82b5afc4fb
alosirgal <147451144+alosirgal@users.noreply.github.com>
2024-03-16 23:17:44 -0500

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -1,5 +1,8 @@
 #ifndef BANK_H
 #define BANK_H
+#include <mutex>
+
+void processTransactions(int deposit, int withdrawal); //For EXP50
 
 void interestHelper(float *interest, int principle, int time, float rate, std::mutex *mutex);
 float conSimpleInterest(int principle, int time, float rate);
