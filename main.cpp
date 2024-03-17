1c05c85da41c4fc09d67344eea330c697221dbca
Brian Nicolellis <briannicolellis@prodigy.net>
2024-03-17 13:07:01 -0500

    Fixed print output

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ int main() {
     {
         if(it >= users.begin() && it < users.end())
         {
-            std::cout << it->getName() << it->getID() << it->getBalance() << it->getPrinciple() << it->getRate() << "\n";
+            std::cout << it->getName() << " " << it->getID() << it->getBalance() << it->getPrinciple() << it->getRate() << "\n";
         }
         else
         {
