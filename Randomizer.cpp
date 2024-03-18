6c6717cabce8715d2aec9bb422c9cb6840cfcb2b
alosirgal <147451144+alosirgal@users.noreply.github.com>
2024-03-17 20:44:22 -0500

    Update Randomizer.cpp

--- Randomizer.cpp
+++ Randomizer.cpp
@@ -3,31 +3,20 @@
 #include <random>
 using namespace std;
 
-/*
-    returns a random value between 0 and 100
-    MSC50
-*/
-// int randomizer()
-// {
-    
-//     static std::random_device rd;
-//     int num = rand();
-//     mt19937 engine(num);
-//     uniform_int_distribution<int> dist(0,100);
-//     return dist(engine);
-// }
-
-/*  
-    Ensure your random number generator is properly seeded
-    MSC51
-*/
+/**
+ *  @brief returns a random value between 0 and 100
+ *  MSC50
+ *  
+ *  @brief Ensure your random number generator is properly seeded
+ *  MSC51
+ * 
+ */
 
 int randomizer()
 {
     static std::random_device rd;
-    // seed the generator
     static std::mt19937 engine(rd());
     std::uniform_int_distribution<int> dist(0,100);
 
-    return dist(engine); //generate a random number
+    return dist(engine);
 }
